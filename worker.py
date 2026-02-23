524faf4f766e145fbaced2bb965c700292332d68
Melon006 <88492848+Melon006@users.noreply.github.com>
2026-02-23 20:36:30 +0530

    Add scan functionality with subprocess commands

--- worker.py
+++ worker.py
@@ -1,1 +1,34 @@
+import subprocess
 
+def run_scan(socketio, target):
+
+    tools = [
+        ("Recon Init", f"ping -c 2 {target}"),
+        ("Nmap Scan", f"nmap -F {target}"),
+        ("Whois Lookup", f"whois {target}")
+    ]
+
+    socketio.emit("update",
+    "[SYSTEM] Initializing Cyber Engine...\n")
+
+    for name, cmd in tools:
+
+        socketio.emit("update",
+        f"\n[+] {name} Started...\n")
+
+        try:
+            result = subprocess.check_output(
+                cmd,
+                shell=True,
+                stderr=subprocess.STDOUT
+            ).decode(errors="ignore")
+
+            socketio.emit("update",
+            result[:2000])
+
+        except:
+            socketio.emit("update",
+            "Execution Failed")
+
+    socketio.emit("update",
+    "\n[SYSTEM] Scan Completed ✓")
