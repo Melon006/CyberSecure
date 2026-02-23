d48156705f5178d5f00dd4b45200b8236def08c1
Melon006 <88492848+Melon006@users.noreply.github.com>
2026-02-23 20:38:17 +0530

    Add background drawing functionality to canvas

--- static/background.js
+++ static/background.js
@@ -1,1 +1,41 @@
+const canvas =
+document.getElementById("bg");
 
+const ctx =
+canvas.getContext("2d");
+
+canvas.width =
+window.innerWidth;
+
+canvas.height =
+window.innerHeight;
+
+function draw(){
+
+ctx.fillStyle=
+"rgba(0,0,20,0.2)";
+
+ctx.fillRect(
+0,0,
+canvas.width,
+canvas.height);
+
+ctx.strokeStyle="#00ffe1";
+
+for(let i=0;i<canvas.width;i+=40){
+ctx.beginPath();
+ctx.moveTo(i,0);
+ctx.lineTo(i,canvas.height);
+ctx.stroke();
+}
+
+for(let j=0;j<canvas.height;j+=40){
+ctx.beginPath();
+ctx.moveTo(0,j);
+ctx.lineTo(canvas.width,j);
+ctx.stroke();
+}
+
+}
+
+setInterval(draw,100);
