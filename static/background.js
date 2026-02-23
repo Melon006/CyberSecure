b8de1606ddedb24ad29db398d41e6b74d865bd01
Melon006 <88492848+Melon006@users.noreply.github.com>
2026-02-23 21:25:19 +0530

    Update background.js

--- static/background.js
+++ static/background.js
@@ -1,41 +1,31 @@
-const canvas =
-document.getElementById("bg");
+const canvas=document.getElementById("bg");
+const ctx=canvas.getContext("2d");
 
-const ctx =
-canvas.getContext("2d");
-
-canvas.width =
-window.innerWidth;
-
-canvas.height =
-window.innerHeight;
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 
 function draw(){
 
-ctx.fillStyle=
-"rgba(0,0,20,0.2)";
-
-ctx.fillRect(
-0,0,
+ctx.fillStyle="rgba(0,10,20,0.2)";
+ctx.fillRect(0,0,
 canvas.width,
 canvas.height);
 
-ctx.strokeStyle="#00ffe1";
+ctx.strokeStyle="#00f7ff22";
 
-for(let i=0;i<canvas.width;i+=40){
+for(let i=0;i<canvas.width;i+=50){
 ctx.beginPath();
 ctx.moveTo(i,0);
 ctx.lineTo(i,canvas.height);
 ctx.stroke();
 }
 
-for(let j=0;j<canvas.height;j+=40){
+for(let j=0;j<canvas.height;j+=50){
 ctx.beginPath();
 ctx.moveTo(0,j);
 ctx.lineTo(canvas.width,j);
 ctx.stroke();
 }
-
 }
 
-setInterval(draw,100);
+setInterval(draw,60);
