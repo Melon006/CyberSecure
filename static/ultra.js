f873a7ef782cefe97a74bd43b9f4af4b7a17a8a7
Melon006 <88492848+Melon006@users.noreply.github.com>
2026-02-23 20:37:25 +0530

    Implement socket communication for scan updates

--- static/ultra.js
+++ static/ultra.js
@@ -1,1 +1,23 @@
+const socket = io();
 
+function startScan(){
+
+let target =
+document.getElementById("target").value;
+
+document.getElementById(
+"terminal").innerHTML="";
+
+socket.emit("start_scan",
+{target:target});
+}
+
+socket.on("update",(msg)=>{
+
+let term =
+document.getElementById("terminal");
+
+term.innerHTML += msg + "<br>";
+term.scrollTop = term.scrollHeight;
+
+});
