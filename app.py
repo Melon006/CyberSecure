6ed08b0f5e15368afa232e3c1b3fb6554f14b2f6
Melon006 <88492848+Melon006@users.noreply.github.com>
2026-02-23 20:36:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from worker import run_scan
+import threading
 
+app = Flask(__name__)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/")
+def home():
+    return render_template("dashboard.html")
+
+@socketio.on("start_scan")
+def start_scan(data):
+
+    target = data["target"]
+
+    thread = threading.Thread(
+        target=run_scan,
+        args=(socketio, target)
+    )
+    thread.start()
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000)
